a2db8056aa4db2157088ea0023e493787de4786e
Pedro Paulo <pedropaulobrasca@gmail.com>
2024-10-11 16:59:47 -0300

    chore: Add .editorconfig file for consistent code formatting

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,5 @@
+import { env } from "./utils/env.ts";
+
 const hello: string = 'Hello, World!!';
 
-console.log(hello);
+console.log({hello, app_name: env.APP_NAME});
---
+++ backend/utils/env.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+import { z } from "zod";
+
+const envSchema = z.object({
+  APP_NAME: z.string().default("mern-auth-service-dev"),
+  PORT: z.string().refine(port => parseInt(port) > 0 && parseInt(port) < 65535, {
+    message: "PORT must be a valid number between 0 and 65535",
+  }).default("3333"),
+  NODE_ENV: z.enum(["development", "production"]).default("development"),
+});
+
+type Env = z.infer<typeof envSchema>;
+
+export const env: Env = envSchema.parse(process.env);
