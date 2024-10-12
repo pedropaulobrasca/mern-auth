44b423c97cba245bf1465c0ab53cd6609bf01ae9
Pedro Paulo <pedropaulobrasca@gmail.com>
2024-10-12 12:33:00 -0300

    feat: Add server health check endpoint

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,20 @@
+import express, { Request, Response } from 'express'
+
 import { env } from './utils/env.ts'
 
-const hello: string = 'Hello, World!!'
+const app = express()
 
-console.log({ hello, app_name: env.APP_NAME })
+const PORT = Number(env.PORT)
+
+app.get('/health', (req: Request, res: Response) => {
+  try {
+    res.status(200).send('Server is running')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Server is down')
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
