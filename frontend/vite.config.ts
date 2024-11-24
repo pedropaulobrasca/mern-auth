f146cae55c7598c402624df66fbac58d5b754114
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-11-24 15:09:14 -0300

    chore: Att proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'https://g6yyayyput.us-east-1.awsapprunner.com',
+      '/api': 'http://localhost:3333',
     },
   },
 })
